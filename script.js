edbf9828c895ec0574665a36bc4814bfcf3bcf77
joe-bor <kimborlongan_08@yahoo.com>
2023-05-01 13:35:23 -0700

    Starts implementing player move function

--- script.js
+++ script.js
@@ -11,17 +11,44 @@ class PlayerShip extends Ship {
     this.type = type; //TODO: Implement in the future? different ships?
   }
 
-  moveShip() {
-    //call this function whenever a key is pressed
-    //!! switch-case VS if-else ?
-    //define moveLeft(), moveRight(), moveUp(), moveDown()
-    //run corresponding function depending on which arrow key is pressed, if not one of the four arrow keys, return
+  //TODO: add 'borders'
+  moveShipLeft() {
+    let playerShipIndex = divTilesArr.indexOf(
+      document.querySelector(".player")
+    );
+    divTilesArr[playerShipIndex].classList.remove("player");
+    divTilesArr[playerShipIndex + -1].classList.add("player");
+  }
+
+  moveShipRight() {
+    let playerShipIndex = divTilesArr.indexOf(
+      document.querySelector(".player")
+    );
+    divTilesArr[playerShipIndex].classList.remove("player");
+    divTilesArr[playerShipIndex + 1].classList.add("player");
+  }
+
+  moveShipUp() {
+    let playerShipIndex = divTilesArr.indexOf(
+      document.querySelector(".player")
+    );
+    divTilesArr[playerShipIndex].classList.remove("player");
+    divTilesArr[playerShipIndex - 32].classList.add("player");
+  }
+
+  moveShipDown() {
+    let playerShipIndex = divTilesArr.indexOf(
+      document.querySelector(".player")
+    );
+    divTilesArr[playerShipIndex].classList.remove("player");
+    divTilesArr[playerShipIndex + 32].classList.add("player");
   }
 
   shootBullet() {
     //call this function when `shift` is pressed
     //on  press, create a new object (define a class for this?)
     //define how it moves, setInterval(ƒ moveBullet)
+    console.log("shoot");
   }
 }
 
@@ -48,20 +75,48 @@ player.bullets;
     CACHED ELEMENTS
 -------------------*/
 const playerDiv = document.querySelector("#player");
-const divTilesArr = document.querySelectorAll(".tiles");
+const divTilesArr = [...document.querySelectorAll(".tiles")];
 
 /*------------------ 
     EVENT LISTENERS 
 -------------------*/
-document.addEventListener("keydown", () => {
-  console.log("pressed");
+//!! switch-case vs if-else ?
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowLeft":
+      player.moveShipLeft();
+      break;
+    case "ArrowRight":
+      player.moveShipRight();
+      break;
+    case "ArrowUp":
+      player.moveShipUp();
+      break;
+    case "ArrowDown":
+      player.moveShipDown();
+      break;
+    case "z":
+      player.shootBullet();
+      break;
+  }
 });
 
 /*------------------ 
     FUNCTIONS 
 ------------------*/
+//!! DELETE this later
 divTilesArr.forEach((divTile, index) => {
   divTile.innerText = index;
 });
 
-//TODO: moveLeft(), moveRight(), moveUp(), moveDown()
+/*
+HOW TO MOVE:
+Find the index of the element that has the class='player' indexOf() + classList.contains()? 
+this would give us the location of our player in the tiles array
+
+then remove class at current location.
+move current index position
+index + 1 =moveRight | index - 1 = moveLeft | index + 32 = moveDown | index -32 = moveUp
+then add class="player" at current position
+render
+*/
