e52a382e73c3e61da787b528a7a66873d3a8a369
joe-bor <kimborlongan_08@yahoo.com>
2023-05-03 18:31:23 -0700

    Starts enemy movement function

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ const MARKER = {
   hero: `<svg xmlns="http://www.w3.org/2000/svg" width="45" height="40" transform="rotate(90)" fill="#FF237a" viewBox="0 0 256 256"><path d="M152,224a8,8,0,0,1-8,8H112a8,8,0,0,1,0-16h32A8,8,0,0,1,152,224Zm71.62-68.17-12.36,55.63a16,16,0,0,1-25.51,9.11L158.51,200h-61L70.25,220.57a16,16,0,0,1-25.51-9.11L32.38,155.83a16.09,16.09,0,0,1,3.32-13.71l28.56-34.26a123.07,123.07,0,0,1,8.57-36.67c12.9-32.34,36-52.63,45.37-59.85a16,16,0,0,1,19.6,0c9.34,7.22,32.47,27.51,45.37,59.85a123.07,123.07,0,0,1,8.57,36.67l28.56,34.26A16.09,16.09,0,0,1,223.62,155.83Zm-139.23,34Q68.28,160.5,64.83,132.16L48,152.36,60.36,208l.18-.13ZM140,100a12,12,0,1,0-12,12A12,12,0,0,0,140,100Zm68,52.36-16.83-20.2q-3.42,28.28-19.56,57.69l23.85,18,.18.13Z"></path></svg>`,
 
   ufo: `<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#000000" viewBox="0 0 256 256"><path d="M183.59,213.47a8,8,0,0,1-15.18,5.06l-8-24a8,8,0,0,1,15.18-5.06ZM128,184a8,8,0,0,0-8,8v32a8,8,0,0,0,16,0V192A8,8,0,0,0,128,184Zm-37.47.41a8,8,0,0,0-10.12,5.06l-8,24a8,8,0,0,0,15.18,5.06l8-24A8,8,0,0,0,90.53,184.41ZM248,112c0,16.22-13.37,30.89-37.65,41.29C188.22,162.78,159,168,128,168s-60.22-5.22-82.35-14.71C21.37,142.89,8,128.22,8,112c0-8.37,3.67-20.79,21.17-32.5,11.37-7.61,26.94-13.76,45.18-17.85A63.64,63.64,0,0,1,173,50.45a64.84,64.84,0,0,1,9.11,11.3C223.43,71.09,248,89.74,248,112ZM176,96a47.66,47.66,0,0,0-6.06-23.35l-.06-.09A48.07,48.07,0,0,0,127.36,48C101.25,48.34,80,70.25,80,96.83v3a7.92,7.92,0,0,0,6.13,7.76A188.24,188.24,0,0,0,128,112a188.09,188.09,0,0,0,41.85-4.37A7.93,7.93,0,0,0,176,99.87Z"></path></svg>`,
-  bullet: "TEST",
+  bullet: "͢",
   0: " ",
 };
 
@@ -91,6 +91,7 @@ let board; //32x18 map, long array
 let player; //player.health, player.score, player.gameOver
 let playerShipIndex; //player's current location in the array
 let enemy; //EnemyShip.enemyNum
+
 /*-------------------
 CACHED ELEMENTS
 -------------------*/
@@ -134,6 +135,9 @@ function init() {
   //! These seem irrelevant now
   player = new PlayerShip(5, false, 0);
 
+  //! TEST
+  board[261] = "bullet";
+
   render();
 }
 
@@ -153,6 +157,22 @@ function renderStats() {
   healthNum.innerText = `${player.health}`;
   scoreNum.innerText = `${player.score}`;
   enemyNum.innerText = `${EnemyShip.enemyNum}`;
+}
+
+function moveEnemyShips() {
+  board.forEach((square, index) => {
+    if (square === "ufo") {
+      board.splice(index, 1, 0);
+      board.splice(index - 1, 1, "ufo");
+    }
+    //locate the index of enemy ships on the board
+    //delete enemy on current index
+    //alter current enemy location
+    //create enemy on new location
+    //render
+    //repeat till certain condition
+  });
+  render();
 }
 
 /*------------------ 
