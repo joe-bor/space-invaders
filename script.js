5ce80e715245a9bb6516280425e72d8c047ebda8
joe-bor <kimborlongan_08@yahoo.com>
2023-05-06 07:23:13 -0700

    Starts implementing shoot function

--- script.js
+++ script.js
@@ -53,7 +53,10 @@ class PlayerShip {
     //on  press, create a new object (define a class for this?)
     //define how it moves, setInterval(ƒ moveBullet)
     console.log("shoot");
-    let bullet = new Bullet(500);
+    let bullet = new Bullet(this.location + 1);
+    board[bullet.location] = "bullet";
+    render();
+    console.log(bullet);
   }
 }
 
@@ -101,8 +104,8 @@ const WIDTH = 32;
 const MARKER = {
   hero: `<svg xmlns="http://www.w3.org/2000/svg" width="45" height="40" transform="rotate(90)" fill="#FF237a" viewBox="0 0 256 256"><path d="M152,224a8,8,0,0,1-8,8H112a8,8,0,0,1,0-16h32A8,8,0,0,1,152,224Zm71.62-68.17-12.36,55.63a16,16,0,0,1-25.51,9.11L158.51,200h-61L70.25,220.57a16,16,0,0,1-25.51-9.11L32.38,155.83a16.09,16.09,0,0,1,3.32-13.71l28.56-34.26a123.07,123.07,0,0,1,8.57-36.67c12.9-32.34,36-52.63,45.37-59.85a16,16,0,0,1,19.6,0c9.34,7.22,32.47,27.51,45.37,59.85a123.07,123.07,0,0,1,8.57,36.67l28.56,34.26A16.09,16.09,0,0,1,223.62,155.83Zm-139.23,34Q68.28,160.5,64.83,132.16L48,152.36,60.36,208l.18-.13ZM140,100a12,12,0,1,0-12,12A12,12,0,0,0,140,100Zm68,52.36-16.83-20.2q-3.42,28.28-19.56,57.69l23.85,18,.18.13Z"></path></svg>`,
 
-  ufo: `<svg xmlns="http://www.w3.org/2000/svg" width="36" height="30" fill="#000000" viewBox="0 0 256 256"><path d="M183.59,213.47a8,8,0,0,1-15.18,5.06l-8-24a8,8,0,0,1,15.18-5.06ZM128,184a8,8,0,0,0-8,8v32a8,8,0,0,0,16,0V192A8,8,0,0,0,128,184Zm-37.47.41a8,8,0,0,0-10.12,5.06l-8,24a8,8,0,0,0,15.18,5.06l8-24A8,8,0,0,0,90.53,184.41ZM248,112c0,16.22-13.37,30.89-37.65,41.29C188.22,162.78,159,168,128,168s-60.22-5.22-82.35-14.71C21.37,142.89,8,128.22,8,112c0-8.37,3.67-20.79,21.17-32.5,11.37-7.61,26.94-13.76,45.18-17.85A63.64,63.64,0,0,1,173,50.45a64.84,64.84,0,0,1,9.11,11.3C223.43,71.09,248,89.74,248,112ZM176,96a47.66,47.66,0,0,0-6.06-23.35l-.06-.09A48.07,48.07,0,0,0,127.36,48C101.25,48.34,80,70.25,80,96.83v3a7.92,7.92,0,0,0,6.13,7.76A188.24,188.24,0,0,0,128,112a188.09,188.09,0,0,0,41.85-4.37A7.93,7.93,0,0,0,176,99.87Z"></path></svg>`,
-  bullet: "͢",
+  ufo: `<svg xmlns="http://www.w3.org/2000/svg" width="36" height="30" fill="#1fa23e" viewBox="0 0 256 256"><path d="M183.59,213.47a8,8,0,0,1-15.18,5.06l-8-24a8,8,0,0,1,15.18-5.06ZM128,184a8,8,0,0,0-8,8v32a8,8,0,0,0,16,0V192A8,8,0,0,0,128,184Zm-37.47.41a8,8,0,0,0-10.12,5.06l-8,24a8,8,0,0,0,15.18,5.06l8-24A8,8,0,0,0,90.53,184.41ZM248,112c0,16.22-13.37,30.89-37.65,41.29C188.22,162.78,159,168,128,168s-60.22-5.22-82.35-14.71C21.37,142.89,8,128.22,8,112c0-8.37,3.67-20.79,21.17-32.5,11.37-7.61,26.94-13.76,45.18-17.85A63.64,63.64,0,0,1,173,50.45a64.84,64.84,0,0,1,9.11,11.3C223.43,71.09,248,89.74,248,112ZM176,96a47.66,47.66,0,0,0-6.06-23.35l-.06-.09A48.07,48.07,0,0,0,127.36,48C101.25,48.34,80,70.25,80,96.83v3a7.92,7.92,0,0,0,6.13,7.76A188.24,188.24,0,0,0,128,112a188.09,188.09,0,0,0,41.85-4.37A7.93,7.93,0,0,0,176,99.87Z"></path></svg>`,
+  bullet: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="40" fill="#d03535" viewBox="0 0 256 256"><path d="M156,128a28,28,0,1,1-28-28A28,28,0,0,1,156,128Z"></path></svg>`,
   0: " ",
 };
 
@@ -127,7 +130,6 @@ for (let i = 0; i <= 575; i++) {
   gameBoard.appendChild(divTilesEl);
 }
 
-//Array of 'squares' on our screen
 const divTilesArr = document.querySelectorAll(".tiles");
 
 const healthNum = document.querySelector("#health-num");
@@ -168,7 +170,6 @@ function render() {
 function renderBoard() {
   board.forEach((square, idx) => {
     divTilesArr[idx].innerHTML = MARKER[square];
-    if (idx <= 31) divTilesArr[idx].style.backgroundColor = "red";
     // divTilesArr[idx].innerText = idx;
   });
 }
@@ -205,37 +206,18 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-/*
-HOW TO SHOOT:
-find player position = playerShipIndex
-find it's center = width/2 ?
-create an element w/ class='bullet' ?
-make it move along the div array ?
-checking for collisions along the way
-  if collision is detected, run collide function () = > {
-    delete bullet (remove class)
-    delete enemy (remove class)
-    explode sounds
-  }
-
-*/
-
-//TODO: Gather Sound Effects
-//TODO: Look for 'logos' or images'
-//TODO: Refactor: playerShipIndex? moveShip functions?
-
 // let moveEnemyID = setInterval(moveEnemy, 500);
 
 function moveEnemy() {
   for (let i = 0; i < enemies.length; i++) {
-    //if at bottom border
+    //bottom border
     if (enemies[i].location > 543) {
       enemies.forEach((enemy) => {
         enemy.moveEnemyShipsLeft();
       });
       isGoingDown = false;
       break;
-      //if at top border
+      //top border
     } else if (enemies[i].location < 32) {
       enemies.forEach((enemy) => {
         enemy.moveEnemyShipsLeft();
@@ -258,18 +240,16 @@ function moveEnemy() {
   }
 }
 
-/**
- call function
-are ships in the bottom border?
-(yes)   call moveLeft function
-  set isGoingDown to false
-  break;
+/*
+!Finish implementing Bullet movement and how it interacts with collisions
+!Set win and lose conditions
+!Create modal -> before playing
+!Gather Sound Effects
+!Look for 'logos' or images'
+!CSS
 
- is it going down?
-    if yes, enemies.reverse()
-    go down function
-    enemies.reverse
 
-    if no,
-    go up function
- */
+
+!Refactor: moveShip functions?
+
+*/
